ae5f39287207e702531b16551db77119e9501998
Rico Dadiz <developer.ricodadiz@gmail.com>
2024-04-05 07:38:16 +0800

    Add plugin main file

---
+++ my-lovely-users-table.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: My Lovely Users Table
+ * Description: Display a table of users from an external API with details fetched asynchronously.
+ * Version: 1.0
+ * Author: Rico Dadiz
+ */
+
+if (!defined('WPINC')) {
+    die;
+}
